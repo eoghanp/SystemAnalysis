68185141f791431e2711fcf63b1a649110d40519
eoghanp <eoghanp95@yahoo.ie>
2016-04-25 20:43:11 +0100

    pre and post conditions added to recipient.java

--- src/Users/Recipient.java
+++ src/Users/Recipient.java
@@ -3,11 +3,23 @@ package Users;
 public interface Recipient {
 	/** @param fname cannot be null and must be a String
 	 * 
-	 * @pre @param fname != null
+	 *  @pre fname != null
 	 */
 	public void setFirstName(String fname);
+	/** @param lname cannot be null and must be a String
+	 * 
+	 *  @pre lname != null
+	 */
 	public void setLastName(String lname);
+	/** @param address cannot be null and must be a String
+	 * 
+	 *  @pre newAddress != null
+	 */
 	public void setAddress(String newAddress);
+	/** @param newTelephone cannot be null and must be a String
+	 * 
+	 *  @pre newTelephone != null 
+	 */
 	public void setTelephone(String newTelephone);
 	public String trackDelivery();
 	/** name cannot be null
@@ -15,6 +27,14 @@ public interface Recipient {
 	 * @post @return name != null
 	 */
 	public String getName();
+	/** address cannot be null
+	 * 
+	 * @post @return address != null
+	 */
 	public String getAddress();
+	/** telephone cannot be null
+	 * 
+	 * @post @return telephone != null
+	 */
 	public String getTelephone();
 }
